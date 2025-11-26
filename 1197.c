1ec53b161e5bbf3348ecc07871f095d6b8975dfe
Flávio <flavio.gallo@unifesp.br>
2025-11-26 20:44:20 -0300

    Create 1197.c

---
+++ 1197.c
@@ -0,0 +1,12 @@
+// EXERCICIO 1076 DO BEECROWD - FLAVIO GALLO CAJAZEIRA
+#include <stdio.h>
+
+int main() {
+    int v, t;
+
+    while (scanf("%d %d", &v, &t) != EOF) {
+        printf("%d\n", v * t * 2);
+    }
+
+    return 0;
+}
